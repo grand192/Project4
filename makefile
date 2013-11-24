f827abf9c4e8f67ba2e1b4d71cc3a7fc45b24a1f
Live session user <ubuntu@ubuntu.(none)>
2013-11-24 13:12:56 -0600

    getting started on server.c

--- makefile
+++ makefile
@@ -2,6 +2,7 @@ CC = gcc
 CFLAGS = -D_REENTRANT
 LDFLAGS = -lpthread
 
+
 all: web_server_http
 
 web_server_http: server.o util.o
--- server.c
+++ server.c
@@ -25,13 +25,17 @@ typedef struct request_queue
 	char	m_szRequest[MAX_REQUEST_LENGTH];
 } request_queue_t;
 
+// Dispatcher thread function
 void * dispatch(void * arg)
 {
+	printf ("dispatcher\n");	
 	return NULL;
 }
 
+// Worker thread function
 void * worker(void * arg)
 {
+	printf ("worker\n");
 	return NULL;
 }
 
@@ -42,8 +46,42 @@ int main(int argc, char **argv)
 	{
 		printf("usage: %s port path num_dispatcher num_workers queue_length [cache_size]\n", argv[0]);
 		return -1;
+	} 
+
+	// Take in the port number and check that it is valid
+	int port = atoi(argv[1]);
+
+	if (port <= 1025 || port >=  65535) {
+		printf("Port number (the first argument to server) must be between 1025 and 65535\n");
+		return -1;
 	}
 
+	// Set up configurations from passed in arguments
+	char* path = argv[2];
+	int num_dispatcher = atoi(argv[3]);
+	int num_workers = atoi(argv[4]);
+	int qlen = atoi(argv[5]);
+	
 	printf("Call init() first and make a dispather and worker threads\n");
+	
+	//Initialize the server with the port number passed in
+	init(port);
+	int num_total_threads = num_dispatcher + num_workers;
+	int i;
+
+	// Create thread ID's starting from 1 to num_dispatcher
+	for (i = 1; i <= num_dispatcher; i++){
+		pthread_t tid = i;
+		pthread_create(&tid, NULL, &dispatch, NULL);
+	}
+
+	// Create unique thread ID's starting from num_dispatcher
+	for (i = num_dispatcher+1; i <= num_total_threads; i++){
+		pthread_t tid = i;
+		pthread_create(&tid, NULL, &worker, NULL);
+	}
+	
+	
+
 	return 0;
 }
