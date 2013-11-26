d5fac1290e30711c41de56a88a613ce801d9525b
AlexanderCook <alex.cook4@yahoo.com>
2013-11-25 20:01:07 -0600

    Committing the server.

--- server.c
+++ server.c
@@ -17,6 +17,7 @@
 #define MAX_THREADS 100
 #define MAX_QUEUE_SIZE 100
 #define MAX_REQUEST_LENGTH 1024
+#define MAX_FILE_SIZE 4096 //4kb
 
 //Structure for queue.
 typedef struct request_queue
@@ -25,10 +26,68 @@ typedef struct request_queue
 	char	m_szRequest[MAX_REQUEST_LENGTH];
 } request_queue_t;
 
-typedef struct Q
-{
-	int is_empty;
-	request_queue_t queue[MAX_QUEUE_SIZE];
+request_queue_t Q[MAX_QUEUE_SIZE];
+pthread_mutex_t q_mtx = PTHREAD_MUTEX_INITIALIZER;
+
+void initialize (request_queue_t *Q){
+	int i;
+	pthread_mutex_lock(&q_mtx);
+	for (i=0; i < MAX_QUEUE_SIZE; i++){
+		Q[i].m_socket = -1; //empty sockets are -1
+	}
+	pthread_mutex_unlock(&q_mtx);
+}
+
+int isEmpty(request_queue_t *Q){
+	int i;
+	int empty;
+	empty = 1;
+	pthread_mutex_lock(&q_mtx);
+	for (i = 0; i < MAX_QUEUE_SIZE; i++){
+		if (Q[i].m_socket != -1){
+			empty=0;
+		}
+	}	
+	pthread_mutex_unlock(&q_mtx);
+	return empty;
+}
+
+int addRequest(request_queue_t request){
+	pthread_mutex_lock(&q_mtx);
+	int i;
+	for (i = 0; i < MAX_QUEUE_SIZE; i++){
+		if (Q[i].m_socket == -1){
+			Q[i].m_socket = request.m_socket;
+			printf("Added a request\n");
+			if(memcpy(Q[i].m_szRequest, request.m_szRequest,  MAX_REQUEST_LENGTH) == NULL){
+				pthread_mutex_unlock(&q_mtx);
+				return -1;
+			}	
+			break;
+		}
+	}
+	pthread_mutex_unlock(&q_mtx);
+	return 0;
+}
+
+int retrieve_request(request_queue_t *request){
+	pthread_mutex_lock(&q_mtx);
+	int i;
+	for (i = 0; i < MAX_QUEUE_SIZE; i++){
+		if (Q[i].m_socket != -1){
+			printf("Retrieved a request\n");
+
+			request->m_socket = Q[i].m_socket;
+			if(memcpy(request->m_szRequest, Q[i].m_szRequest,  MAX_REQUEST_LENGTH) == NULL){
+				pthread_mutex_unlock(&q_mtx);
+				return -1;
+			}	
+			Q[i].m_socket = -1;
+			return 1; //shows we retrieved a request
+		}
+	}
+	pthread_mutex_unlock(&q_mtx);
+	return 0;
 }
 
 // Dispatcher thread function
@@ -37,7 +96,10 @@ void * dispatch(void * arg)
 	request_queue_t request;
 	while(1){
 		request.m_socket = accept_connection(); 
-		get_request(request.m_socket, request.m_szRequest); 
+		get_request( request.m_socket, request.m_szRequest); 
+		if ( addRequest(request) == -1){
+			printf("The request could not be added by dispatcher of pthread_id: %d\n", pthread_self());
+		}
 	}
 }
 
@@ -45,7 +107,7 @@ int parseContentType(char* file){
 	int len = strlen(file);
 	char substr[5];
 	strncpy(substr, file+(len-6), 5);//not sure if this will work right
-	if ( strcmp(substr, ".html") || strcmp(substr+1 , ".htm")
+	if ( strcmp(substr, ".html") || strcmp(substr+1 , ".htm") )
 		return 0;
 	else if ( strcmp(substr+1 , ".jpg") )
 		return 1;
@@ -58,8 +120,36 @@ int parseContentType(char* file){
 void * worker(void * arg)
 {
 	request_queue_t job;
+	char buf[MAX_FILE_SIZE];
+	int bytes_read;
+	int num_jobs = 0;
+
 	while(1){
-		return_result(job.m_socket, "text/html", /*location of read in file*/, /*amount read in from file*/);
+		if( retrieve_request (&job)) {
+			if( bytes_read = read(job.m_socket, buf, MAX_FILE_SIZE) == -1){
+				perror("File could not be read.");
+			}
+			else {
+				switch ( parseContentType(job.m_szRequest) )
+				{
+					case 0:
+						return_result(job.m_socket, "text/html", buf, bytes_read);
+						break;
+					case 1:
+						return_result(job.m_socket, "image/jpeg", buf, bytes_read);
+						break;
+					case 2:
+						return_result(job.m_socket, "image/gif", buf, bytes_read);
+						break;
+					case 3:
+						return_result(job.m_socket, "text/plain", buf, bytes_read);
+						break;
+					default:
+						perror("retrieve_request failed miserably.");
+				}
+			num_jobs++; //increment job count
+			}
+		}
 	}
 }
 
@@ -87,26 +177,42 @@ int main(int argc, char **argv)
 	int num_workers = atoi(argv[4]);
 	int qlen = atoi(argv[5]);
 	
+	if ( chdir(path) == -1){
+		perror("Failed to change directories. Exit.");
+		exit(-1);
+	}
+
 	printf("Call init() first and make a dispather and worker threads\n");
 	
 	//Initialize the server with the port number passed in
 	init(port);
 	int num_total_threads = num_dispatcher + num_workers;
 	int i;
+	initialize(Q);
+
+	/*
+	printf("Q's first value: %d\n", Q[0].m_socket );
+	printf("Is it empty? : %d\n", isEmpty(Q));
+	Q[0].m_socket = 5;
+	printf("Is it empty? : %d\n", isEmpty(Q));
+	*/
+
 
 	// Create thread ID's starting from 1 to num_dispatcher
 	for (i = 1; i <= num_dispatcher; i++){
-		pthread_t tid = i;
+		pthread_t tid;
+		tid = i;
 		pthread_create(&tid, NULL, &dispatch, NULL);
 	}
 
 	// Create unique thread ID's starting from num_dispatcher
 	for (i = num_dispatcher+1; i <= num_total_threads; i++){
-		pthread_t tid = i;
+		pthread_t tid;
+		tid = i;
 		pthread_create(&tid, NULL, &worker, NULL);
 	}
 	
-	
+	while(1);
 
 	return 0;
 }
